648a330535ee79676fde9cd5130d31e418322466
tehcuapm <tehcuapm@gmail.com>
2023-07-28 11:49:28 +0200

    push

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,16 +1,18 @@
 class TodoItemsController < ApplicationController
 
+  before_action do
+    @todo_list = TodoList.find(params[:todo_list_id])
+
+  end
+
   def new
-    @todo_item = TodoItem.new
-    @todo_list_id = params[:todo_list_id]
+    @todo_item = @todo_list.todo_items.build
   end
 
   def create
-    @todo_list_id = params[:todo_list_id]
-    @todo_item = TodoItem.new todo_item_params
-    @todo_item.assign_attributes(todo_list_id: @todo_list_id)
+    @todo_item = @todo_list.todo_items.build todo_item_params
     if @todo_item.save
-      redirect_to todo_list_path(@todo_list_id)
+      redirect_to todo_list_path(@todo_list)
     else
       render 'new'
     end
--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,12 +1,15 @@
 class TodoListsController < ApplicationController
+
+  before_action only: [:update, :edit, :show] do
+    @todo_list = TodoList.find(params[:id])
+
+  end
+
   def index
     @todo_lists = TodoList.all
   end
 
-  def show
-    @todo_list = TodoList.find(params[:id])
-    @todo_item = TodoItem.new
-  end
+  def show ; end
 
   def new
     @todo_list = TodoList.new
@@ -21,12 +24,9 @@ class TodoListsController < ApplicationController
     end
   end
 
-  def edit
-    @todo_list = TodoList.find(params[:id])
-  end
+  def edit ; end
 
   def update
-    @todo_list = TodoList.find(params[:id])
     @todo_list.assign_attributes todo_list_params
     if @todo_list.save
       redirect_to todo_list_path(@todo_list)
