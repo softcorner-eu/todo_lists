383baf35145e22ea4f26b37a1418e3afb80620b2
tehcuapm <tehcuapm@gmail.com>
2023-09-12 10:16:00 +0200

    [FIX]: fix and refacto tag_names=

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -14,19 +14,20 @@ class TodoList < ApplicationRecord
   has_and_belongs_to_many :tags
 
   accepts_nested_attributes_for :todo_items, reject_if: :all_blank
-  accepts_nested_attributes_for :tags, reject_if: :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :tags, reject_if: :all_blank
 
   def tag_names
     tags.pluck(:name)
   end
 
   def tag_names=(tag_names_array)
+    tags.each do |tag|
+      tag.destroy unless tag.name.in?(tag_names_array)
+    end
     tag_names_array.each do |tag_name|
-      tag = Tag.find_or_create_by(name: tag_name)
-      tags << tag
+      tags.build(name: tag_name) unless tags.find {|t| t.name == tag_name }
     end
   end
-
 
   def self.human_enum_name(enum_name, enum_value)
     I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name.to_s.pluralize}.#{enum_value}")
